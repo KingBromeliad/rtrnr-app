f1cfaaab4e0494f8917930234e8510dce515b6ae
Bromeliad <busi.samuele99@gmail.com>
2021-07-07 22:33:08 +0200

    ui adjustements

--- src/views/History.vue
+++ src/views/History.vue
@@ -15,51 +15,54 @@
         </ion-text>
       </ion-item>
     </ion-header>
-    <ion-content>
-    <div
-      style="justify-content: flex-start; height: 100vh"
-      class="rounded-container-bottom"
-    >
-      <ion-item v-if="pastWorkouts.length == 0" lines="none" color="light">
-        <ion-text color="dark">
-          <h4
-            style="font-weight: 400; font-size: 1.5em; margin-top: 0.4em; line-height: 1.3em"
+    <ion-content color="light">
+      <div style="justify-content: flex-start; height: 100vh">
+        <ion-item v-if="pastWorkouts.length == 0" lines="none" color="light">
+          <ion-text color="dark">
+            <h4
+              style="font-weight: 400; font-size: 1.5em; margin-top: 0.4em; line-height: 1.3em"
+            >
+              Completed workouts will be shown here
+            </h4>
+          </ion-text>
+        </ion-item>
+        <div id="history-grid">
+          <ion-card
+            v-for="pastWorkout in pastWorkouts"
+            :key="pastWorkout.timestamp"
+            button="true"
+            @click="goToWorkout(pastWorkout)"
+            style="border-radius: 28px; width: 46vw; margin: 0.5em"
+            :style="{ '--background': pastWorkout.color }"
           >
-            Completed workouts will be shown here
-          </h4>
-        </ion-text>
-      </ion-item>
-      <div id="history-grid">
-        <ion-card
-          v-for="pastWorkout in pastWorkouts"
-          :key="pastWorkout.timestamp"
-          button="true"
-          @click="goToWorkout(pastWorkout)"
-          style="border-radius: 28px;"
-          :color="pastWorkout.color"
-        >
-          <ion-item lines="none" :color="pastWorkout.color">
-            <div class="info-box">
-              <h1
-                style="font-weight: 550; font-size: 1.8em; margin-bottom: 0.1em; margin-top: 0.1em; color: var(--ion-color-light);"
-              >
-                {{ pastWorkout.name }}
-              </h1>
-              <h6
-                style="font-weight: 300; font-size: 1em; margin-top: 0.1em; margin-bottom: 0.5em; color: var(--ion-color-light);"
-              >
-                {{ pastWorkout.timestamp }}
-              </h6>
-            </div>
-            <ion-icon
-              style="color: var(--ion-color-light)"
-              :icon="chevronForward"
-            ></ion-icon>
-          </ion-item>
-        </ion-card>
+            <ion-item
+              lines="none"
+              :style="{ '--background': pastWorkout.color }"
+            >
+              <div class="info-box">
+                <h1
+                  style="font-weight: 550; font-size: 1.8em; margin-bottom: 0.1em; margin-top: 0.1em; color: var(--ion-color-light);"
+                >
+                  {{ pastWorkout.name }}
+                </h1>
+                <h6
+                  style="font-weight: 300; font-size: 1em; margin-top: 0.1em; margin-bottom: 0.5em; color: var(--ion-color-light);"
+                >
+                  {{ pastWorkout.timestamp }}
+                </h6>
+              </div>
+              <ion-icon
+                style="color: var(--ion-color-light)"
+                :icon="chevronForward"
+              ></ion-icon>
+            </ion-item>
+          </ion-card>
+        </div>
       </div>
-    </div>
     </ion-content>
+    <ion-footer>
+      <div class="rounded-container-bottom"></div>
+    </ion-footer>
   </ion-page>
 </template>
 
@@ -73,6 +76,7 @@ import {
   IonModal,
   IonIcon,
   IonContent,
+  IonFooter,
 } from "@ionic/vue";
 import { defineComponent, onMounted, ref, reactive, onUpdated } from "vue";
 import { auth, db } from "../main";
@@ -91,6 +95,7 @@ export default defineComponent({
     IonModal,
     IonIcon,
     IonContent,
+    IonFooter,
   },
   setup() {
     const type: object[] = [];
@@ -143,14 +148,11 @@ export default defineComponent({
 </script>
 <style scoped>
 #history-grid {
-  padding-left: 1vw;
-  padding-right: 1vw;
-  display: grid;
-  width: 100vw;
-  grid-template-columns: 1fr 1fr;
-  grid-auto-rows: 1fr;
-  gap: 0rem;
-  justify-items: center;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: space-between;
+  background-color: var(--ion-color-light);
 }
 .title {
   background-color: var(--ion-color-primary-contrast);
@@ -158,10 +160,9 @@ export default defineComponent({
   padding-left: 1vh;
 }
 .rounded-container-bottom {
+  display: block;
   background-color: var(--ion-color-light);
-  height: 100vh;
-  padding-top: 20px;
-  padding-bottom: 10px;
+  height: 21px;
   border-bottom-left-radius: 20px;
   border-bottom-right-radius: 20px;
 }
